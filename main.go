74289ecd074ffd1d04451c4bdc97ecc06157cacb
Almaz <almaztx@gmail.com>
2026-07-31 21:00:32 +0500

    Слайсы

--- main.go
+++ main.go
@@ -1,62 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
-type User struct {
-	Name    string
-	Rating  float64
-	Premium bool
-}
+import "fmt"
 
 func main() {
-	user1 := User{
-		Name:    "Almaz",
-		Rating:  5.5,
-		Premium: true,
-	}
-	user2 := User{
-		Name:    "Shyngys",
-		Rating:  5.5,
-		Premium: false,
-	}
-	user3 := User{
-		Name:    "Muhamedzhan",
-		Rating:  5.5,
-		Premium: true,
-	}
-	user4 := User{
-		Name:    "Dias",
-		Rating:  5.5,
-		Premium: false,
-	}
-
-	users := [4]User{user1, user2, user3, user4}
-
-	for index, user := range users {
-		fmt.Println(index, user)
-
-		if user.Premium {
-			users[index].Rating += 1.0
-		}
-
-		fmt.Println(users)
-		fmt.Println("")
-	}
-
-	for _, user := range users {
-		fmt.Println(user)
-	}
-	// for i := 0; i < len(users); i++ {
-
-	// 	fmt.Println(users[i])
-
-	// 	if users[i].Premium {
-	// 		users[i].Rating += 1.0
-	// 	}
-
-	// 	fmt.Println(users[i])
-	// 	fmt.Println("")
-	// }
+	intSlice := make([]int, 0, 5)
+	intSlice = append(intSlice, 10, 12, 13, 143)
+	fmt.Println(intSlice, len(intSlice), cap(intSlice))
 }
